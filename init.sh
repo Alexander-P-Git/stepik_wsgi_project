63a61efae6a8950dcb8b1764bb8be21928b67e9b
Alexander-P-Git <43542042+Alexander-P-Git@users.noreply.github.com>
2019-03-23 17:24:27 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,5 +3,5 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/wsgi.example
 sudo /etc/init.d/gunicorn restart
-sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:app
+
 sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask:app
