4821dfcdf66aac7710ae5c4eab5818e88d7cda05
Alexander-P-Git <43542042+Alexander-P-Git@users.noreply.github.com>
2019-04-09 18:19:29 +0300

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     url(r'^$', include('qa.urls')),
     url(r'^login/', include('qa.urls')),
     url(r'^signup/', include('qa.urls')),
-    url(r'^question/<111>/', include('qa.urls')),
+    url(r'^question/', include('qa.urls')),
     url(r'^ask/', include('qa.urls')),
     url(r'^popular/', include('qa.urls')),
     url(r'^view/', include('qa.urls')),    
